ed1ec3459a9589c0012887ea0b75ffb33f15a710
MikeKragelundDK <kragelund-wow@hotmail.com>
2025-09-21 11:09:57 +0200

    Initial commit

--- src/main/java/dk/databases/Migrations_week39/MigrationsWeek39Application.java
+++ src/main/java/dk/databases/Migrations_week39/MigrationsWeek39Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MigrationsWeek39Application {
-		// test
+		// test2
 	public static void main(String[] args) {
 		SpringApplication.run(MigrationsWeek39Application.class, args);
 	}
