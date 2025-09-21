4cf7a05bb60c25f96a6febc46ce02f44dde7fd77
MikeKragelundDK <kragelund-wow@hotmail.com>
2025-09-21 11:30:52 +0200

    Entities done.

---
+++ src/main/java/dk/databases/Migrations_week39/entities/Course.java
@@ -0,0 +1,20 @@
+package dk.databases.Migrations_week39.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "id",unique = true)
+    private int id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "credits")
+    private int credits;
+}
---
+++ src/main/java/dk/databases/Migrations_week39/entities/Student.java
@@ -0,0 +1,26 @@
+package dk.databases.Migrations_week39.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",unique = true)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "enrollment_date")
+    private LocalDate EnrollmentDate;
+}
