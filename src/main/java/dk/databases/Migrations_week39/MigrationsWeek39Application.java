61a4672faef5b2e07a8fd7749c1994eed74132ca
MikeKragelundDK <kragelund-wow@hotmail.com>
2025-09-21 11:08:12 +0200

    Initial commit

---
+++ src/main/java/dk/databases/Migrations_week39/MigrationsWeek39Application.java
@@ -0,0 +1,13 @@
+package dk.databases.Migrations_week39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationsWeek39Application {
+		// test
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationsWeek39Application.class, args);
+	}
+
+}
---
+++ src/test/java/dk/databases/Migrations_week39/MigrationsWeek39ApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.databases.Migrations_week39;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationsWeek39ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
